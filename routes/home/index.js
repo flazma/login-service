2ab26324ca7093fde21a1a15443ddd87f408f8bd
flazma <flazma@naver.com>
2022-11-01 11:16:22 +0900

    sencond commit

---
+++ routes/home/index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const { Router } = require('express');
+const express = require('express');
+const router = express.Router();
+
+const ctrl = require("./home.ctrl");
+
+
+router.get("/",ctrl.home);
+
+router.get("/login",ctrl.login);
+
+module.exports = router;
+
+
